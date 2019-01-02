8de6a48d6e4eeae861bd89400cc2d49a9b8b55cb
annacv <21217131+annacv@users.noreply.github.com>
2019-01-02 21:13:52 +0100

    Add new dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,22 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'babel/semi': 0,
+    'generator-star-spacing': 'off',
+    'indent': [ 2, 2, {'SwitchCase': 1} ],
+    'lines-between-class-members': [ 'error', 'always' ],
+    'space-before-blocks': 'error',
+    'lines-around-directive': [ 'error', 'always' ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/order-in-components': 0,
+    'vue/max-attributes-per-line': [ 2, {
+      'singleline': 2,
+      'multiline': {
+        'max': 2,
+        'allowFirstLine': false
+      }
+    } ]
   }
 }
