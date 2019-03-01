3f6a290c809489be83412a766e89e48a88eea5a4
annacv <21217131+annacv@users.noreply.github.com>
2019-03-01 18:00:05 +0100

    [global] import cover component, remove contact c

--- src/languages/es/index.js
+++ src/languages/es/index.js
@@ -1,13 +1,13 @@
-import contact from './contact'
 import contract from './contract'
+import cover from './cover'
 import highlights from './highlights'
 import meta from './meta'
 import summary from './summary'
 import synopsis from './synopsis'
 
 export default {
-  contact,
   contract,
+  cover,
   highlights,
   meta,
   summary,
