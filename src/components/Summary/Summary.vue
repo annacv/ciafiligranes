4fd36f5d14511ed3586ceb33591fe2bb9c3e9b9e
annacv <21217131+annacv@users.noreply.github.com>
2019-03-12 14:17:14 +0100

    [CIAF-16] Refactor to fix summary in grid

--- src/components/Summary/Summary.vue
+++ src/components/Summary/Summary.vue
@@ -4,33 +4,37 @@
 </style>
 
 <template>
-  <b-container class="summary">
-    <b-row>
-      <b-col cols="md-7">
-        <p
-          class="summary__abstract"
-          v-text="abstract"
-        />
-      </b-col>
-      <b-col cols="md-5">
-        <ul
-          v-for="item in items"
-          :key="item.title"
-          class="summary__list"
-        >
-          <li class="summary__list-item">
+  <b-container fluid class="summary">
+    <b-row class="justify-content-center">
+      <div class="col-12 col-sm-10 col-lg-10 col-xl-9 col-xxl-7 col-xxxl-6">
+        <b-row class="justify-content-center">
+          <b-col md="8" lg="8" xl="7">
             <p
-              class="summary__item--bold"
-              v-text="item.title"
+              class="summary__abstract"
+              v-text="abstract"
             />
-            <p
-              class="summary__item--light"
-              v-text="item.description"
-            />
-          </li>
-        </ul>
-        <!--b-button block>Dossier</b-button-->
-      </b-col>
+          </b-col>
+          <b-col md="4" lg="4" xl="5">
+            <ul
+              v-for="item in items"
+              :key="item.title"
+              class="summary__list"
+            >
+              <li class="summary__list-item">
+                <p
+                  class="summary__item--bold"
+                  v-text="item.title"
+                />
+                <p
+                  class="summary__item--light"
+                  v-text="item.description"
+                />
+              </li>
+            </ul>
+            <!--b-button block>Dossier</b-button-->
+          </b-col>
+        </b-row>
+      </div>
     </b-row>
   </b-container>
 </template>
