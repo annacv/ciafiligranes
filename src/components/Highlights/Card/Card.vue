a617f13594ba732c871773f2de53290b67dedb18
annacv <21217131+annacv@users.noreply.github.com>
2019-03-15 19:26:11 +0100

    [CIAF-8] Drama trying to set img as props

---
+++ src/components/Highlights/Card/Card.vue
@@ -0,0 +1,59 @@
+<!--style
+  lang="scss"
+  src="./card.scss"
+  scoped>
+</style-->
+
+<template>
+  <div class="card">
+    <img
+      :src="require(`~/assets${image}`)"
+      :alt="alt"
+      class="card__image"
+    >
+    <a
+      :href="link"
+      class="card__link"
+    >
+      <div class="card__content">
+        <h4
+          class="card__title"
+          v-text="title"
+        />
+        <p
+          class="card__name"
+          v-text="name"
+        />
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+
+  props: {
+    image: {
+      type: String,
+      default: ''
+    },
+    alt: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
