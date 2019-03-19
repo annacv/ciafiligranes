c5199da0bc24351f66cef0e042e921a49377b6ca
annacv <21217131+annacv@users.noreply.github.com>
2019-03-19 19:51:21 +0100

    [summary] Fix en

--- src/languages/en/summary.js
+++ src/languages/en/summary.js
@@ -1,6 +1,6 @@
 export default {
   abstract:
-    'The key to this work is a constant game of tug-of-war, which gives rise to an impregnated relationship of fantasy, similar to the real world in which children live, at the same time intriguing and conflicting. Among the Clown (Makutu ) and August (Toti) there is a relationship where carefully curb their rivalry and complicity',
+    'The key to this work is a constant game of tug-of-war, which gives rise to an impregnated relationship of fantasy, similar to the real world in which children live, at the same time intriguing and conflicting. Among the Clown (Makutu) and August (Toti) there is a relationship where carefully curb their rivalry and complicity',
   items: [
     {
       title: 'Plis-plas',
