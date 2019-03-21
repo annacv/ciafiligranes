daf4963c0fd8a7d472aa0e0136ddad59b53b623c
annacv <21217131+annacv@users.noreply.github.com>
2019-03-21 13:56:22 +0100

    [CIAF-25] LanguageSelectorWorkingOK

---
+++ src/components/TheHeader/components/LanguageSelector.vue
@@ -0,0 +1,51 @@
+<style
+  lang="scss"
+  src="../theHeader.scss"
+  scoped
+>
+</style>
+
+<template>
+  <div class="language-selector">
+    <label
+      aria-label="Select language"
+      class="language-selector__label"
+      for="select-lang"
+    >
+    </label>
+    <select
+      id="select-lang"
+      class="language-selector__field"
+      @change="changeLocale()"
+    >
+      <option selected>
+        {{ $i18n.locale }}
+      </option>
+      <option
+        v-for="locale in availableLocales"
+        :key="locale.code" >{{ locale.code }}
+        <nuxt-link
+          :to="switchLocalePath(locale.code)">{{ locale.name }}
+        </nuxt-link>
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LanguageSelector',
+
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    }
+  },
+
+  methods: {
+    changeLocale() {
+      this.$router.push(this.switchLocalePath(this.selected))
+    }
+  }
+}
+</script>
