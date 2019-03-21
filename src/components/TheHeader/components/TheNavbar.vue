f991ae60bff034cb43c203e8f060d84b38760dd0
annacv <21217131+annacv@users.noreply.github.com>
2019-03-21 13:55:42 +0100

    [CIAF-24] Move navbar component

---
+++ src/components/TheHeader/components/TheNavbar.vue
@@ -0,0 +1,54 @@
+<style
+  lang="scss"
+  src="../theHeader.scss"
+  scoped
+>
+</style>
+
+<template>
+  <div class= "header__navbar">
+    <nav
+      class="navbar__main"
+      role="navigation"
+      aria-label="main navigation">
+        <div class="grid-col-12">
+          <ul>
+            <li
+              v-for="(item, key) of items"
+              :key="key"
+              class="navbar__menu-item">
+              <nuxt-link
+                :to="item.to"
+                exact-active-class="is-active">
+                {{ item.title }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+        <LanguageSelector/>
+    </nav>
+  </div>
+</template>
+
+<script>
+import LanguageSelector from '../components/LanguageSelector'
+
+export default {
+  name: 'TheNavbar',
+
+  components: {
+    LanguageSelector
+  },
+
+  data() {
+    return {
+      items: [
+        {
+          title: 'Contactar',
+          to: { name: 'contactar' }
+        }
+      ]
+    }
+  }
+}
+</script>
