39f882a01664ae6295ec05709b62767aa076acab
annacv <21217131+annacv@users.noreply.github.com>
2019-03-22 17:59:37 +0100

    [CIAF-17] summary responsive, pending 768

--- src/components/Synopsis/Synopsis.vue
+++ src/components/Synopsis/Synopsis.vue
@@ -9,7 +9,7 @@
   <section class="main-content">
     <b-container fluid class="synopsis__heading">
       <b-row no-gutters>
-        <div class="col-12 col-sm-10 col-lg-10 col-xl-10 col-xxl-8 col-xxxl-6 offset-1 offset-xxl-2">
+        <div class="col-12 col-md-12 col-lg-10 col-xl-10 col-xxl-8 col-xxxl-6 offset-0 offset-md-1 offset-xxl-2">
           <h3
             class="synopsis__title"
             v-text="title"
