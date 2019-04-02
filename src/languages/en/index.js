aab8b2a31a1c9f5bb10537fb9e893b6cec9ae652
annacv <21217131+annacv@users.noreply.github.com>
2019-04-02 14:54:07 +0200

    [global] and fix it in en es OMG

--- src/languages/en/index.js
+++ src/languages/en/index.js
@@ -1,5 +1,7 @@
 import contract from './contract'
 import cover from './cover'
+import footer from './footer'
+import header from './header'
 import highlights from './highlights'
 import meta from './meta'
 import summary from './summary'
@@ -8,6 +10,8 @@ import synopsis from './synopsis'
 export default {
   contract,
   cover,
+  footer,
+  header,
   highlights,
   meta,
   summary,
