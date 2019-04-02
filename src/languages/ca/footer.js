05a25575c591c4e2623dba43d47d9aec38268da1
annacv <21217131+annacv@users.noreply.github.com>
2019-04-02 22:27:17 +0200

    [CIAF-3] Content added in languages

--- src/languages/ca/footer.js
+++ src/languages/ca/footer.js
@@ -1,3 +1,54 @@
 export default {
-  credits: 'credits'
+  socialicons: [
+    {
+      alt: 'Facebook de la Companyia Filigranes. Obre en una nova pestanya',
+      image: '/icons/facebook.svg',
+      link: 'https://www.facebook.com/Cia-Filigranes-204888372926384/'
+    },
+    {
+      alt: 'Instagram de la Companyia Filigranes. Obre en una nova pestanya',
+      image: '/icons/instagram.svg',
+      link: 'https://www.instagram.com/cia.filigranes/'
+    },
+    {
+      alt:
+        'Canal Youtube de la Companyia Filigranes. Obre en una nova pestanya',
+      image: '/icons/youtube.svg',
+      link: 'https://www.youtube.com/channel/UCaH3Bb7VIiuzwy51TyyKzpg'
+    }
+  ],
+
+  email: {
+    arialabel:
+      "Fent clic s'obre una finestra nova per escriure un correu electrònic",
+    href: 'mailto:ciafiligranes@gmail.com',
+    link: 'ciafiligranes@gmail.com'
+  },
+
+  adress: 'c/ St. Leopold, 63. Local 5. 08221. Terrassa (Barcelona)',
+
+  logo: 'Logo de la Companyia Filigranes',
+
+  cia: 'Companyia Filigranes, 2019',
+
+  credits: [
+    {
+      text: 'Disseny i desenvolupament web:',
+      arialabel: "Fent clic s'obre una finestra nova",
+      href: 'https://ar.linkedin.com/in/anna-condal-vela',
+      link: 'Anna Condal'
+    },
+    {
+      text: 'Fotografia:',
+      arialabel: "Fent clic s'obre una finestra nova",
+      href: 'https://es.linkedin.com/in/gerard-bañó-4283162a',
+      link: 'Gerard Bañó'
+    },
+    {
+      text: 'i',
+      arialabel: "Fent clic s'obre una finestra nova",
+      href: 'http://www.martagc.com/',
+      link: 'Marta Garcia'
+    }
+  ]
 }
