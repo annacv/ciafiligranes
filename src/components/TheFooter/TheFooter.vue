8c3a4a6ac2c563c553f7163634ae38eb25a027e7
annacv <21217131+annacv@users.noreply.github.com>
2019-04-03 17:23:42 +0200

    [CIAF-3] Fix footer large screens

--- src/components/TheFooter/TheFooter.vue
+++ src/components/TheFooter/TheFooter.vue
@@ -15,7 +15,7 @@
             class="footer__image"
           >
         </div>
-        <div class="col-12 col-md-4 col-lg-6 footer__contact">
+        <div class="col-12 col-md-4 col-lg-6 col-xxl-4 col-xxxl-5 footer__contact">
           <b-row no-gutters align-v="center" align-h="center">
             <a
               :aria-label="email.arialabel"
@@ -29,7 +29,7 @@
             />
           </b-row>
         </div>
-        <div class="col-12 col-md-3 col-lg-2 offset-0 offset-md-1 offset-xxl-2 footer__rrss">
+        <div class="col-12 col-md-3 col-lg-2 offset-0 offset-md-1 offset-xxl-2 offset-xxxl-2 footer__rrss">
           <ul class="social-icons__list">
             <li
               v-for="item in socialicons"
