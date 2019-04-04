c9ce52669fff2a99d9a4189ecc0aa1003a1bb680
annacv <21217131+annacv@users.noreply.github.com>
2019-04-04 22:07:13 +0200

    [components] Add button component and styles

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,40 @@
+<style
+  lang="scss"
+  src="./button.scss"
+  scoped
+>
+</style>
+
+<template>
+  <a
+    :class="button.class"
+    :type="button.type"
+    :href="button.href"
+    role="button"
+    download="CiaFiligranes-PlisPlas_dossier-ca.pdf"
+    target="__blank"
+  >
+    <slot>
+      {{ button.text }}
+      <img
+        v-if="button.slot.alt"
+        :alt="button.slot.alt"
+        :src="require(`~/assets/icons/${button.slot.class}.svg`)"
+        :class="button.slot.class"
+      >
+    </slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+
+  props: {
+    button: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
