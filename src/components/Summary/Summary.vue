bdc2b92d14d133b00ad916d1911430b22f008230
annacv <21217131+annacv@users.noreply.github.com>
2019-04-05 16:13:56 +0200

    [CIAF-17] Fix btn in summary for lg screens

--- src/components/Summary/Summary.vue
+++ src/components/Summary/Summary.vue
@@ -10,13 +10,13 @@
     <b-row no-gutters>
       <div class="col-12 col-md-10 col-lg-10 col-xl-10 col-xxl-8 col-xxxl-8 offset-0 offset-md-1 offset-xxl-2">
         <b-row no-gutters>
-          <div class="col-12 col-md-7 col-lg-8 col-xl-7 col-xxl-7 col-xxxl-7">
+          <div class="col-12 col-md-7 col-lg-7 col-xl-7 col-xxl-7 col-xxxl-7">
             <p
               class="summary__abstract"
               v-text="abstract"
             />
           </div>
-          <div class="col-12 col-md-5 col-lg-3 col-xl-4 col-xxl-4 col-xxxl-4 offset-lg-1">
+          <div class="col-12 col-md-5 col-lg-5 col-xl-4 col-xxl-4 col-xxxl-4 offset-xl-1">
             <ul
               v-for="item in items"
               :key="item.title"
