07fb4ddee8de7c9f54b65119969179829835c5f8
annacv <21217131+annacv@users.noreply.github.com>
2019-04-12 19:09:50 +0200

    CIAF-meta_add_pending_meta_content_ca_es_en

--- src/languages/es/meta.js
+++ src/languages/es/meta.js
@@ -1,14 +1,32 @@
 export default {
   index: {
-    title: 'Companyia Filigranes',
+    title:
+      'Compañía de payasos Filigranes. Encuentra espectáculos, talleres y animaciones a tu medida.',
+    link: [
+      {
+        rel: 'canonical',
+        href: 'http://ciafiligranes.net/'
+      }
+    ],
     meta: [
       {
         property: 'description',
-        content: ''
+        content:
+          '¡Bienvenidas a la Compañía Filigranes! Makutu y Trinxeta os acercamos al mundo del circo y los payasos haciendo disfrutar a toda la familia de nuestros espectáculos.'
+      },
+      {
+        property: 'og:url',
+        content: 'http://ciafiligranes.net/'
+      },
+      {
+        property: 'og:title',
+        content:
+          'Compañía de payasos Filigranes. Encuentra espectáculos, talleres y animaciones a tu medida.'
       },
       {
         property: 'og:description',
-        content: ''
+        content:
+          '¡Bienvenidas a la Compañía Filigranes! Makutu y Trinxeta os acercamos al mundo del circo y los payasos haciendo disfrutar a toda la familia de nuestros espectáculos.'
       }
     ]
   }
