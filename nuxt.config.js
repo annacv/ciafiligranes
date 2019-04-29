3f7d67901403a345830c473228c02e88dd3ff8f8
annacv <21217131+annacv@users.noreply.github.com>
2019-04-29 17:39:38 +0200

    [Config] Fix links on head

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,47 +22,6 @@ module.exports = {
     ],
     link: [
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: 'favicon/favicon.ico'
-      },
-      {
-        rel: 'shortcut icon',
-        href: 'favicon/favicon.ico?filigranes=WGozkoE8Mx'
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: 'favicon/apple-touch-icon.png?filigranes=WGozkoE8Mx'
-      },
-      {
-        rel: 'icon',
-        sizes: '32x32',
-        type: 'image/png',
-        href: 'favicon//favicon-32x32.png?filigranes=WGozkoE8Mx'
-      },
-      {
-        rel: 'icon',
-        sizes: '192x192',
-        type: 'image/png',
-        href: 'favicon//android-chrome-192x192.png?filigranes=WGozkoE8Mx'
-      },
-      {
-        rel: 'icon',
-        sizes: '16x16',
-        type: 'image/png',
-        href: 'favicon/favicon-16x16.png?filigranes=WGozkoE8Mx'
-      },
-      {
-        rel: 'manifest',
-        href: 'favicon/site.webmanifest?filigranes=WGozkoE8Mx'
-      },
-      {
-        rel: 'mask-icon',
-        href: 'favicon/safari-pinned-tab.svg?filigranes=WGozkoE8Mx',
-        color: '#bc1b36'
-      },
-      {
         rel: 'preload',
         as: 'font',
         href: '/fonts/akkurat/akkurat_light_regular-webfont.woff2',
@@ -82,6 +41,43 @@ module.exports = {
         href: '/fonts/akkurat/akkurat_bold-webfont.woff2',
         type: 'font/woff2',
         crossorigin: 'crossorigin'
+      },
+      {
+        rel: 'shortcut icon',
+        type: 'image/x-icon',
+        href: 'favicon/favicon.ico'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: 'favicon/apple-touch-icon.png?filigranes=WGozkoE8Mx'
+      },
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/png',
+        href: 'favicon/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        sizes: '192x192',
+        type: 'image/png',
+        href: 'favicon/android-chrome-192x192.png'
+      },
+      {
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png',
+        href: 'favicon/favicon-16x16.png'
+      },
+      {
+        rel: 'mask-icon',
+        href: 'favicon/safari-pinned-tab.svg',
+        color: '#bc1b36'
+      },
+      {
+        rel: 'manifest',
+        href: '/manifest.json'
       }
     ]
   },
