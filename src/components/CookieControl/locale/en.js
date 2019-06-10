eea00daf8ddc19a1561fd226814ff1b1da4f311e
annacv <21217131+annacv@users.noreply.github.com>
2019-06-10 18:50:49 +0200

    [cookies] remove unneeded

--- src/components/CookieControl/locale/en.js
+++
@@ -1,6 +0,0 @@
-export default {
-  barTitle: 'Cookies',
-  barDescription:
-    'We use our own cookies and third-party cookies so that we can show you this website and better understand how you use it, with a view to improving the services we offer. If you continue browsing, we consider that you have accepted the cookies.',
-  acceptAll: 'Accept all'
-}
