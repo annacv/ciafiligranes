bff22526d0c8992f723b265b0d54821f914bd3a1
annacv <21217131+annacv@users.noreply.github.com>
2019-06-10 12:10:33 +0200

    [NC] Create dotted list component

---
+++ src/languages/en/dottedList.js
@@ -0,0 +1,55 @@
+export default {
+  cookies: {
+    items: [
+      {
+        title: 'Type of cookies',
+        subtitle: 'Technical cookies:',
+        paragraph:
+          'These cookies allow the User to browse a web page, platform or application and to use the different options or services provided, such as monitoring traffic and data reporting, identification of session, access to restricted sections, using safety features during browsing, storing content for the streaming of videos or sound or sharing content via the social media.'
+      },
+      {
+        subtitle: 'Customisation cookies:',
+        paragraph:
+          'These cookies allow the User to access the service with certain predefined general characteristics based on a set of criteria in the User’s device or machine, such as language, the type of browser through which the service is accessed, the regional settings from where the service is accessed, etc.'
+      },
+      {
+        subtitle: 'Analytical cookies:',
+        paragraph:
+          'These cookies allow the person responsible for them to monitor and analyse the behaviour of users on the websites to which they are linked. The information collected through this type of cookies is used to measure the activity of the websites, application or platform and to generate browsing profiles of the users of these sites, applications and platforms in order to make improvements based on the analysis of the usage data of the users of the service.'
+      },
+      {
+        subtitle: 'Advertising cookies:',
+        paragraph:
+          'These cookies allow for the most efficient management possible of any advertising spaces that the publisher has included on a website, application or platform from which the service is provided, based on criteria such as published content or the frequency with which adverts are displayed.'
+      },
+      {
+        subtitle: 'Behavioural advertising cookies:',
+        paragraph:
+          "These cookies allow for the most efficient management of advertising spaces included by the publisher on a website, application or platform from which the service requested is provided. These cookies store information about the users' behaviour obtained from the continuous observation of their browsing habits, enabling the development of a specific profile for showing advertising based on these habits."
+      },
+      {
+        title: 'Deactivating and uninstalling cookies',
+        paragraph:
+          'Most computers automatically accept the use of cookies. However, the User can decide whether or not to continue enabling cookies on their computer. Disabling cookies may mean the website to operate suboptimally, disabling some of the characteristics or services provided, e.g. making it impossible to register, among others. Nevertheless, and even after all cookies have been disabled, the web browser will continue to collect certain essential information for the basic functioning of the website. Users can disable cookies from the preferences section on their Internet browser. Here are some links provided by the most popular browsers, with instructions for managing cookies on different devices and versions, if required.'
+      },
+      {
+        href: 'https://support.google.com/chrome/answer/95647?hl=en',
+        link: 'Google Chrome'
+      },
+      {
+        href:
+          'https://support.microsoft.com/help/17442/windows-internet-explorer-delete-manage-cookies',
+        link: 'Internet Explorer'
+      },
+      {
+        href:
+          'https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences',
+        link: 'Mozilla Firefox'
+      },
+      {
+        href: 'https://support.apple.com/en-gb/HT201265',
+        link: 'Safari iPhone, iPad and iPod touch and Safari for Mac'
+      }
+    ]
+  }
+}
---
+++ src/languages/es/dottedList.js
@@ -0,0 +1,55 @@
+export default {
+  cookies: {
+    items: [
+      {
+        title: 'Tipologías de cookies',
+        subtitle: 'Cookies técnicas:',
+        paragraph:
+          'Son aquellas que permiten a la usuaria la navegación a través de un sitio web, plataforma o aplicación y la utilización de las diferentes opciones o servicios que en ella existan como, por ejemplo, controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a partes de acceso restringido, utilizar elementos de seguridad durante la navegación, almacenar contenidos para la difusión de videos o sonido o compartir contenidos a través de redes sociales.'
+      },
+      {
+        subtitle: 'Cookies de personalización:',
+        paragraph:
+          'Son aquellas que permiten a la usuaria acceder al servicio con algunas características generales predefinidas en función de una serie de criterios en el terminal de la usuaria, como por ejemplo serían el idioma, el tipo de navegador a través del cual accede al servicio, la configuración regional desde donde accede al servicio, etc.'
+      },
+      {
+        subtitle: 'Cookies de análisis:',
+        paragraph:
+          'Son aquellas que permiten al responsable de las mismas, el seguimiento y análisis del comportamiento de las ususarias de los sitios web a los que están vinculadas. La información recogida mediante este tipo de cookies se utiliza en la medición de la actividad de los sitios web, aplicación o plataforma y para la elaboración de perfiles de navegación de las ususarias de dichos sitios, aplicaciones y plataformas, con el fin de introducir mejoras en función del análisis de los datos de uso que hacen las ususarias del servicio.'
+      },
+      {
+        subtitle: 'Cookies publicitarias',
+        paragraph:
+          'Son aquellas que permiten la gestión, de la forma más eficaz posible, de los espacios publicitarios que, en su caso, el editor haya incluido en un sitio web, aplicación o plataforma desde la que presta el servicio solicitado en base a criterios como el contenido editado o la frecuencia en la que se muestran los anuncios.'
+      },
+      {
+        subtitle: 'Cookies de publicidad comportamental:',
+        paragraph:
+          'Son aquellas que permiten la gestión, de la forma más eficaz posible, de los espacios publicitarios que, en su caso, el editor haya incluido en un sitio web, aplicación o plataforma desde la que presta el servicio solicitado. Estas cookies almacenan información del comportamiento de las ususarias obtenida a través de la observación continuada de sus hábitos de navegación, lo que permite desarrollar un perfil específico para mostrar publicidad en función del mismo.'
+      },
+      {
+        title: 'Desactivación y desinstalación de las cookies',
+        paragraph:
+          'La mayoría de ordenadores aceptan la utilización de cookies automáticamente. Sin embargo, la usuaria tiene la posibilidad de decidir si mantiene las cookies habilitadas o no en su ordenador. El hecho de deshabilitar las cookies puede implicar la pérdida de un funcionamiento óptimo del Sitio Web, quedando inhabilitadas algunas de las características o servicios prestados por la misma(por ejemplo, imposibilidad de registrarse, entre otras). Con todo, incluso habiendo desactivado todas las cookies, el navegador web recoge determinada información esencial para el funcionamiento básico del Sitio Web. La usuaria que desee deshabilitar las cookies puede hacerlo desde la sección de preferencias de su navegador de Internet. A continuación, indicamos los enlaces habilitados por los navegadores más populares, con las instrucciones para gestionar las cookies, en diversos dispositivos y versiones si es necesario.'
+      },
+      {
+        href: 'https://support.google.com/chrome/answer/95647?hl=es',
+        link: 'Google Chrome'
+      },
+      {
+        href:
+          'https://support.microsoft.com/es-es/help/17442/windows-internet-explorer-delete-manage-cookies',
+        link: 'Internet Explorer'
+      },
+      {
+        href:
+          'https://support.mozilla.org/es/kb/habilitar-y-deshabilitar-cookies-sitios-web-rastrear-preferencias',
+        link: 'Mozilla Firefox'
+      },
+      {
+        href: 'https://support.apple.com/es-es/HT201265',
+        link: 'Safari iPhone, iPad y iPod touch y Safari para Mac'
+      }
+    ]
+  }
+}
