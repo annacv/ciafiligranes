e01b8378b2c1a7897d1627bcf16bab8daa643688
annacv <21217131+annacv@users.noreply.github.com>
2019-06-10 12:09:18 +0200

    [NC] Create plain text component

---
+++ src/components/PlainText/PlainText.vue
@@ -0,0 +1,46 @@
+<style
+  lang="scss"
+  src="./plainText.scss"
+  scoped
+>
+</style>
+
+<template>
+  <b-container fluid class="plain-text">
+    <b-row no-gutters>
+      <div class="col-12 col-md-10 col-lg-10 col-xl-10 col-xxl-8 col-xxxl-8 offset-0 offset-md-1 offset-xxl-2">
+        <b-row no-gutters>
+          <div
+            v-for="item in items"
+            :key="item.paragraph"
+            class="plain-text__content"
+          >
+            <h2
+              v-if="item.title"
+              class="plain-text__title"
+              v-text="item.title"
+            />
+            <p
+              class="plain-text__paragraph"
+              v-text="item.paragraph"
+            />
+          </div>
+        </b-row>
+      </div>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'PlainText',
+
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  }
+}
+</script>
