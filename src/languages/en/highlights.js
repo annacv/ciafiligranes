c59b0a41232239e5004bd892fad98720c743764f
annacv <21217131+annacv@users.noreply.github.com>
2019-06-14 12:20:36 +0200

    [highlights] New content and styles for shows page

--- src/languages/en/highlights.js
+++ src/languages/en/highlights.js
@@ -79,5 +79,91 @@ export default {
         }
       }
     ]
+  },
+
+  shows: {
+    title: 'More Filigranes',
+    description: 'Find à la carte performances and workshops for all ages',
+    highlighted: [
+      {
+        title: 'Workshops',
+        name: 'Circus',
+        link: '',
+        image: '/images/img_thumbnail_circ.jpg',
+        alt: 'An image of the Circus Workshop',
+        button: {
+          class: 'button-outline-secondary button-outline-secondary__teal',
+          type: 'application',
+          href: '/downloads/CiaFiligranes-CircusWorkshop.pdf',
+          target: '__blank',
+          download: 'CiaFiligranes-CircusWorkshop.pdf',
+          text: 'Dossier',
+          slot: {
+            alt: 'Arrow down. By clicking a file will be downloaded',
+            class: 'arrow_down teal'
+          }
+        }
+      },
+
+      {
+        title: 'Workshop',
+        name: 'Soap Bubbles',
+        link: '',
+        image: '/images/img_thumbnail_bombolles.jpg',
+        alt: 'An image of The Soap Bubbles Workshop',
+        button: {
+          class: 'button-outline-secondary button-outline-secondary__teal',
+          type: 'application',
+          href: '/downloads/CiaFiligranes-SoapBubblesWorkshop.pdf',
+          target: '__blank',
+          download: 'CiaFiligranes-SoapBubblesWorkshop.pdf',
+          text: 'Dossier',
+          slot: {
+            alt: 'Arrow down. By clicking a file will be downloaded',
+            class: 'arrow_down teal'
+          }
+        }
+      },
+
+      {
+        title: 'Performances',
+        name: 'à la carte',
+        link: '',
+        image: '/images/img_thumbnail_animacions.jpg',
+        alt: 'Image of the animation with juggling of fire',
+        button: {
+          class: 'button-outline-secondary button-outline-secondary__teal',
+          type: 'application',
+          href: '/downloads/CiaFiligranes-Performances.pdf',
+          target: '__blank',
+          download: 'CiaFiligranes-Performances.pdf',
+          text: 'Dossier',
+          slot: {
+            alt: 'Arrow down. By clicking a file will be downloaded',
+            class: 'arrow_down teal'
+          }
+        }
+      },
+
+      {
+        title: 'Workshop',
+        name: 'Clowns',
+        link: '',
+        image: '/images/img_thumbnail_pallassos.jpg',
+        alt: 'An image of The Clowns Workshop',
+        button: {
+          class: 'button-outline-secondary button-outline-secondary__teal',
+          type: 'application',
+          href: '/downloads/CiaFiligranes-ClownsWorkshop.pdf',
+          target: '__blank',
+          download: 'CiaFiligranes-ClownsWorkshop.pdf',
+          text: 'Dossier',
+          slot: {
+            alt: 'Arrow down. By clicking a file will be downloaded',
+            class: 'arrow_down teal'
+          }
+        }
+      }
+    ]
   }
 }
