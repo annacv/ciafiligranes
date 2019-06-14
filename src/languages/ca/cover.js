a2f845983d82e9aff538059b58f1a25b320c4716
annacv <21217131+annacv@users.noreply.github.com>
2019-06-14 12:15:32 +0200

    [cover] Refactor to suit new content, add new styles

--- src/languages/ca/cover.js
+++ src/languages/ca/cover.js
@@ -3,6 +3,10 @@ export default {
     claim: 'En un Plis-Plas…',
     title: 'amistat, rivalitat i jocs poca-soltes'
   },
+  shows: {
+    claim: 'Cia.Filigranes presenta...',
+    title: 'Pallassos i circ per a totes!'
+  },
   legalDisclaimer: {
     title: 'Avís legal'
   },
