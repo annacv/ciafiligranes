381df06e331e0e96579d52661bad6dcaf7fbd86b
annacv <21217131+annacv@users.noreply.github.com>
2019-06-14 12:14:02 +0200

    [shows] Create index page and routes

---
+++ src/pages/espectacles/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="content">
+    <Cover
+      :content="$t('cover.shows')"
+      class="cover__products products__shows"
+    />
+    <section class="main-content">
+      <ProductList
+        :items="$t('productList.shows.items')"
+        class="product-list__shows"
+      />
+    </section>
+    <Contract
+      :title="$t('contract.shows.title')"
+      :items="$t('contract.items')"
+      class="contract__shows"
+    />
+    <Highlights
+      :content="$t('highlights.shows')"
+      :highlighted="$t('highlights.shows.highlighted')"
+      class="highlights__mixed"
+    />
+  </div>
+</template>
+
+<script>
+import Cover from '@/components/Cover/Cover'
+import Contract from '@/components/Contract/Contract'
+import Highlights from '@/components/Highlights/Highlights'
+import ProductList from '@/components/ProductList/ProductList'
+
+export default {
+  name: 'Shows',
+
+  components: {
+    Contract,
+    Cover,
+    Highlights,
+    ProductList
+  },
+
+  nuxtI18n: {
+    paths: {
+      es: '/espectaculos',
+      ca: '/espectacles',
+      en: '/shows'
+    }
+  }
+}
+</script>
