685298dbb6bf8b2f4f561d486e2a199223304b9f
annacv <21217131+annacv@users.noreply.github.com>
2019-06-18 22:42:58 +0200

    [cover] Workshops content & styles

--- src/languages/ca/cover.js
+++ src/languages/ca/cover.js
@@ -3,13 +3,21 @@ export default {
     claim: 'En un Plis-Plas…',
     title: 'amistat, rivalitat i jocs poca-soltes'
   },
+
   shows: {
     claim: 'Cia.Filigranes presenta...',
     title: 'Pallassos i circ per a totes!'
   },
+
+  workshops: {
+    claim: 'Cia.Filigranes us ofereix',
+    title: 'tallers per a totes les edats'
+  },
+
   legalDisclaimer: {
     title: 'Avís legal'
   },
+
   cookies: {
     title: 'Política de cookies'
   }
--- src/languages/es/cover.js
+++ src/languages/es/cover.js
@@ -3,13 +3,21 @@ export default {
     claim: 'En un Plis-Plas…',
     title: 'amistad, rivalidad y juegos pillines'
   },
+
   shows: {
     claim: 'Cia.Filigranes presenta...',
     title: '¡Payasos y circo para a todas!'
   },
+
+  workshops: {
+    claim: 'Cia.Filigranes os ofrece',
+    title: 'talleres para todas las edades'
+  },
+
   legalDisclaimer: {
     title: 'Aviso legal'
   },
+
   cookies: {
     title: 'Política de cookies'
   }
