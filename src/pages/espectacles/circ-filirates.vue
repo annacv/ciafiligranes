2f29c859d530e4868eb88c3084f549987074418b
annacv <21217131+annacv@users.noreply.github.com>
2019-09-02 14:45:38 +0200

    Shows pages

---
+++ src/pages/espectacles/circ-filirates.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="content">
+    <Cover
+      :content="$t('cover.filirates')"
+      class="cover__products-detail shows__filirates"
+    />
+    <section class="main-content">
+      <Summary
+        :abstract="$t('summary.filirates.abstract')"
+        :items="$t('summary.filirates.items')"
+        :button="$t('summary.filirates.button')"
+      />
+      <DataSheet
+        :titles="$t('dataSheet.filirates.titles')"
+        :sheet1="$t('dataSheet.filirates.sheet1')"
+        :sheet2="$t('dataSheet.filirates.sheet2')"
+        class="shows"
+      />
+    </section>
+    <Gallery
+      :images="$t('gallery.filirates.images')"
+    />
+    <Contract
+      :title="$t('contract.title')"
+      :items="$t('contract.items')"
+      class="contract__shows-detail"
+    />
+    <Highlights
+      :content="$t('highlights.filirates')"
+      :highlighted="$t('highlights.filirates.highlighted')"
+      class="highlights__shows"
+    />
+  </div>
+</template>
+
+<script>
+import Cover from '@/components/Cover/Cover'
+import Contract from '@/components/Contract/Contract'
+import DataSheet from '@/components/DataSheet/DataSheet'
+import Gallery from '@/components/Gallery/Gallery'
+import Highlights from '@/components/Highlights/Highlights'
+import Summary from '@/components/Summary/Summary'
+
+export default {
+  name: 'CircFilirates',
+
+  components: {
+    Contract,
+    Cover,
+    DataSheet,
+    Gallery,
+    Highlights,
+    Summary
+  },
+
+  nuxtI18n: {
+    paths: {
+      es: '/espectaculos/circo-filirates',
+      ca: '/espectacles/circ-filirates',
+      en: '/shows/filirates-circus'
+    }
+  }
+}
+</script>
