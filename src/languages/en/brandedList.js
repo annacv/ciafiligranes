87a2df1a25e7d7330c9794367ded66443e65b26b
annacv <21217131+annacv@users.noreply.github.com>
2019-09-20 17:11:12 +0200

    Fix eng in branded list

--- src/languages/en/brandedList.js
+++ src/languages/en/brandedList.js
@@ -36,7 +36,7 @@ export default {
             'https://www.google.com/maps/place/Cia+Filigranes/@41.5678,2.0105131,17z/data=!3m1!4b1!4m5!3m4!1s0x12a492c3e2155299:0x4d072d8f6b4f6768!8m2!3d41.567796!4d2.0127018',
           aria:
             "By clicking you will open google maps with the headquarter's location.",
-          text: 'c/ St. Leopold, 63. Local 5. 08221. Terrassa (Barcelona)'
+          text: 'St. Leopold Street, 63. Local 5. 08221. Terrassa (Barcelona)'
         }
       ]
     }
