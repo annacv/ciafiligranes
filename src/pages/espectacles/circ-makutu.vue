a1f103d3c0f20d3f1503f861d40f9184a56c7271
annacv <acondalv@gmail.com>
2021-04-13 19:02:21 +0200

    Add new show's pages

---
+++ src/pages/espectacles/circ-makutu.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="content">
+    <Cover
+      :content="$t('cover.makutu')"
+      class="cover__shows-detail shows__makutu"
+    />
+    <section class="main-content">
+      <Summary
+        :abstract="$t('summary.makutu_circus.abstract')"
+        :items="$t('summary.makutu_circus.items')"
+      />
+      <DataSheet
+        :titles="$t('dataSheet.titles')"
+        :sheet1="$t('dataSheet.makutu_circus.sheet1')"
+        :sheet2="$t('dataSheet.makutu_circus.sheet2')"
+        class="shows one_clown"
+      />
+    </section>
+    <Gallery
+      :images="$t('gallery.makutu_circus.images')"
+    />
+    <Contract
+      :title="$t('contract.title')"
+      :items="$t('contract.items')"
+      class="contract__shows-detail"
+    />
+    <Highlights
+      :content="$t('highlights.filirates')"
+      :highlighted="$t('highlights.filirates.highlighted')"
+      :button="$t('highlights.showsDetail.button')"
+      class="highlights__shows"
+    />
+  </div>
+</template>
+
+<script>
+import Cover from '@/components/Cover/Cover'
+import Contract from '@/components/Contract/Contract'
+import DataSheet from '@/components/DataSheet/DataSheet'
+import Gallery from '@/components/Gallery/Gallery'
+import Highlights from '@/components/Highlights/Highlights'
+import Summary from '@/components/Summary/Summary'
+
+export default {
+  name: 'CircFilirates',
+
+  components: {
+    Contract,
+    Cover,
+    DataSheet,
+    Gallery,
+    Highlights,
+    Summary
+  },
+
+  nuxtI18n: {
+    paths: {
+      es: '/espectaculos/circo-makutu',
+      ca: '/espectacles/circ-makutu',
+      en: '/shows/makutu-circus'
+    }
+  }
+}
+</script>
---
+++ src/pages/espectacles/circ-trinxeta.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="content">
+    <Cover
+      :content="$t('cover.trinxeta')"
+      class="cover__shows-detail shows__trinxeta"
+    />
+    <section class="main-content">
+      <Summary
+        :abstract="$t('summary.trinxeta_circus.abstract')"
+        :items="$t('summary.trinxeta_circus.items')"
+      />
+      <DataSheet
+        :titles="$t('dataSheet.titles')"
+        :sheet1="$t('dataSheet.trinxeta_circus.sheet1')"
+        :sheet2="$t('dataSheet.trinxeta_circus.sheet2')"
+        class="shows one_clown"
+      />
+    </section>
+    <Gallery
+      :images="$t('gallery.trinxeta_circus.images')"
+    />
+    <Contract
+      :title="$t('contract.title')"
+      :items="$t('contract.items')"
+      class="contract__shows-detail"
+    />
+    <Highlights
+      :content="$t('highlights.trinxeta_circus')"
+      :highlighted="$t('highlights.trinxeta_circus.highlighted')"
+      :button="$t('highlights.showsDetail.button')"
+      class="highlights__shows"
+    />
+  </div>
+</template>
+
+<script>
+import Cover from '@/components/Cover/Cover'
+import Contract from '@/components/Contract/Contract'
+import DataSheet from '@/components/DataSheet/DataSheet'
+import Gallery from '@/components/Gallery/Gallery'
+import Highlights from '@/components/Highlights/Highlights'
+import Summary from '@/components/Summary/Summary'
+
+export default {
+  name: 'CircFilirates',
+
+  components: {
+    Contract,
+    Cover,
+    DataSheet,
+    Gallery,
+    Highlights,
+    Summary
+  },
+
+  nuxtI18n: {
+    paths: {
+      es: '/espectaculos/circo-trinxeta',
+      ca: '/espectacles/circ-trinxeta',
+      en: '/shows/trinxeta-circus'
+    }
+  }
+}
+</script>
