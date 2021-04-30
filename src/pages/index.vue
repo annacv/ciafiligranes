194b5a148de2194f0ec864356c3a14383c374219
annacv <annaconda981@gmail.com>
2021-04-30 21:25:32 +0200

    update index with more content and synopsis rf

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,9 +11,9 @@
         :button="$t('summary.home.button')"
       />
       <Synopsis
-        :title="$t('synopsis.home.title')"
         :image="$t('synopsis.home.image')"
         :paragraphs="$t('synopsis.home.paragraphs')"
+        :buttons="$t('synopsis.home.buttons')"
         class="synopsis__home"
       />
       <Contract
@@ -27,12 +27,24 @@
       :highlighted="$t('highlights.home.highlighted')"
       class="highlights__mixed"
     />
+    <section>
+      <Filipartners
+        :heading="$t('filipartners.heading')"
+        :partnersicons="$t('filipartners.partnersicons')"
+      />
+      <BrandedList
+        :brandedicons="$t('brandedList.brandedCorp')"
+        class="branded-list-corp"
+      />
+    </section>
   </div>
 </template>
 
 <script>
+import BrandedList from '@/components/BrandedList/BrandedList'
 import Cover from '@/components/Cover/Cover'
 import Contract from '@/components/Contract/Contract'
+import Filipartners from '@/components/Filipartners/Filipartners'
 import Highlights from '@/components/Highlights/Highlights'
 import Summary from '@/components/Summary/Summary'
 import Synopsis from '@/components/Synopsis/Synopsis'
@@ -45,8 +57,10 @@ export default {
   },
 
   components: {
+    BrandedList,
     Contract,
     Cover,
+    Filipartners,
     Highlights,
     Summary,
     Synopsis
@@ -54,8 +68,8 @@ export default {
 
   nuxtI18n: {
     paths: {
-      es: '/',
       ca: '/',
+      es: '/',
       en: '/'
     }
   }
